5078b07720ed6eb68c2063c88108b939c2ba0c7b
tainfante <agatadul@gmail.com>
2018-01-22 19:41:39 +0100

    Improved layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Time to Digital Converter");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 325, 275));
         primaryStage.show();
     }
 
